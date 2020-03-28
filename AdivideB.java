3da4a95ae3f514cef98d57c743881a3167e922a7
whdkwhdk90 <62650790+whdkwhdk90@users.noreply.github.com>
2020-03-28 22:21:12 +0900

    백준 1일차 연습문제풀이

---
+++ AdivideB.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class AdivideB{
+    public static void main(String[] args){
+        double a, b;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        
+        System.out.println(a/b);
+        
+    }
+}
---
+++ AmultipleB.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class AmultipleB{
+    public static void main(String[] args){
+        int a, b;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a*b);
+        
+    }
+}
