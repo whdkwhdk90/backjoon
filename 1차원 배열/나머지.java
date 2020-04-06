30dea709f400aefa56c86ac9c719b7f8aa90165f
whdkwhdk90 <62650790+whdkwhdk90@users.noreply.github.com>
2020-04-06 20:22:36 +0900

    백준 4.6

---
+++ 1차원 배열/나머지.java
@@ -0,0 +1,5 @@
+package backjoon;
+
+public class 나머지 {
+
+}
---
+++ 실습1/별찍기9.java
@@ -0,0 +1,41 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class  별찍기9 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+for(int i=1; i<=2*num-1;i++) 
+			{ 
+				if(i<=num) {
+					for(int j = 1; j <= 2*num-1; j++) 
+					{
+							if(j<i || j>2*num-i) {
+								System.out.print(" ");
+							}else {
+								System.out.print("*");
+							}
+					}	
+				}
+				else{
+					for(int j = 1; j <= 2*num-1; j++) 
+					{
+							if(j>i || j<2*num-i) {
+								System.out.print(" ");
+							}else {
+								System.out.print("*");
+							}
+					}	
+				}		
+				System.out.println();
+				
+			}
+	}
+	
+		
+	
+	
+}
